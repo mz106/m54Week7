b7e5894da2fa8df057f5d5c5623da0adee0dfabe
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-02-06 14:24:22 +0000

    enhancement: add stuff

--- src/server.js
+++ src/server.js
@@ -12,14 +12,43 @@ const fakeArr = [];
 
 app.use(express.json());
 
-app.get("/getAllBooks", (request, response) => {
+app.get("/books", (request, response) => {
+  console.log("/books: ", request.path);
   response.send({ message: "success", fakeArr: fakeArr });
 });
 
-app.post("/addBook", (request, response) => {
+app.get("/books/getfirstbook", (request, response) => {
+  // get te first book
+  console.log("/books/books: ", request.path);
+  const book = fakeArr[0];
+  response.send({ message: "success", book: book });
+});
+
+app.post("/books", (request, response) => {
+  console.log("title: ", request.body.title);
+  console.log("genre: ", request.body.genre);
+  console.log("author: ", request.body.author);
+
   fakeArr.push(request.body);
 
+  let awesome;
+  for (let i = 0; i < fakeArr.length; i++) {
+    if (fakeArr[i].title === request.body.title) {
+      awesome = "it's awsome";
+    }
+  }
+  console.log(awesome);
   response.send({ message: "success", newBook: fakeArr[fakeArr.length - 1] });
+});
+
+app.put("/books", (request, reponse) => {
+  // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
+  // change (update) the author to an new name
+});
+
+app.delete("/books", (request, response) => {
+  // in here, find a book by title (i.e. an element of fakeArr where the element title is the same as request.body.title)
+  // remove (delete) the element from the array
 });
 
 app.listen(5001, () => {
