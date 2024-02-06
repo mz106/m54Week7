3a685f145ee00884a6cce3a64699b2cc5710f713
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-02-06 11:14:13 +0000

    chore: cleanup

--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
