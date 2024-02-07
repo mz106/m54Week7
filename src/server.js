49518b721365291c6524bad4a56b55adf0a7bd70
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-02-07 12:30:41 +0000

    another commit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
+
+const connection = async () => {
+  await mongoose.connect(
+    "mongodb+srv://michael:b8y6ZekZY5HNPQEm@cluster0.ntmsxeg.mongodb.net/m54Week7"
+  );
+  console.log("DB connection is working");
+};
+
+connection();
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+  },
+  genre: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
 
 app.get("/books", (request, response) => {});
 
